f17c977740dd24e269b9f08254753d4516b816a7
prodevtem <prodevtem@gmail.com>
2021-11-21 07:05:07 -0800

    first commit

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,82 +1,57 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
-const scene: THREE.Scene = new THREE.Scene();
+const scene = new THREE.Scene();
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 var light = new THREE.PointLight();
-light.position.set(2.5, 7.5, 15);
+light.position.set(5, 15, 30);
 scene.add(light);
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 3;
-
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+const progressBar = document.getElementById('progressBar');
 
-const mtlLoader = new MTLLoader();
-mtlLoader.load(
-  'models/monkey.mtl',
-  (materials) => {
-    materials.preload();
-
-    const objLoader: OBJLoader = new OBJLoader();
-    objLoader.setMaterials(materials);
-    objLoader.load(
-      'models/Surface-Insole.obj',
-      (object) => {
-        scene.add(object);
-      },
-      (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-      },
-      (error) => {
-        console.log('An error happened');
-      }
-    );
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-  },
-  (error) => {
-    console.log('An error happened');
-  }
-);
+const objLoader = new OBJLoader();
+objLoader.load('models/Map1.obj', (object) => {
+    progressBar.style.display = "none";
+    scene.add(object);
+}, (xhr) => {
+    if (xhr.lengthComputable) {
+        var percentComplete = xhr.loaded / xhr.total * 100;
+        progressBar.value = percentComplete;
+        progressBar.style.display = "block";
+    }
+}, (error) => {
+    console.log(error);
+});
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
 }
-
 const stats = Stats();
 document.body.appendChild(stats.dom);
 
 var animate = function () {
-  requestAnimationFrame(animate);
-
-  controls.update();
-
-  render();
-
-  stats.update();
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+    stats.update();
 };
-
 function render() {
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 animate();
